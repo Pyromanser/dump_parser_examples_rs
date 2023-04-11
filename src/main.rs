377ff17db10f2e8c299d3aa343dc96565afdb362
yaroslav <qwaswexexortinvoke@gmail.com>
2023-04-11 22:55:36 +0300

    small modifications

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,26 @@ const SITE_BASE_URL: &str = "http://translatedby.com";
 const TAG: &str = "GURPS";
 const TAG_URL: &str = "http://translatedby.com/you/tags/GURPS/";
 
+fn get_pages_urls(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut pages_urls: Vec<String> = vec![url.to_owned()];
+
+    let text = reqwest::blocking::get(url).expect("Server error").text()?;
+    let document = Html::parse_document(&text);
+    let selector = Selector::parse(r#"div.spager a"#)?;
+
+    pages_urls.extend(
+        document
+            .select(&selector)
+            .map(|a| {
+                let url = a.value().attr("href").ok_or("No href attribute")?;
+                Ok(format!("{}{}", SITE_BASE_URL, url))
+            })
+            .collect::<Result<Vec<_>, Box<dyn Error>>>()?,
+    );
+
+    Ok(pages_urls)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!(
         "Starting dumping {} at {}",
@@ -20,23 +40,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let dump_dir_name: String = format!("{}_{}_rust", TAG, timestamp);
     DirBuilder::new().create(&dump_dir_name)?;
 
-    let text = reqwest::blocking::get(TAG_URL)?.text()?;
-
-    let document = Html::parse_document(&text);
-    let selector = Selector::parse(r#"div.spager a"#)?;
-
-    let mut pages_urls: Vec<String> = vec![TAG_URL.to_owned()];
-
-    pages_urls.extend(
-        document
-            .select(&selector)
-            .map(|a| {
-                let url = a.value().attr("href").ok_or("No href attribute")?;
-                Ok(format!("{}{}", SITE_BASE_URL, url))
-            })
-            .collect::<Result<Vec<_>, Box<dyn Error>>>()?,
-    );
-
+    let pages_urls = get_pages_urls(TAG_URL)?;
     pages_urls
         .iter()
         .map(|url| parse_page(url, &dump_dir_name))
@@ -51,14 +55,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_page(url: &str, dump_dir_name: &str) {
+fn parse_page(url: &str, dump_dir_name: &str) -> Result<(), Box<dyn Error>> {
     println!("Page: {}", url);
 
-    let resp = reqwest::blocking::get(url).expect("Server error");
-    let text = resp.text().expect("Unable to read response");
+    let text = reqwest::blocking::get(url)?.text()?;
 
     let document = Html::parse_document(&text);
-    let selector = Selector::parse(r#"dl.translations-list dt a"#).expect("Selector error");
+    let selector = Selector::parse(r#"dl.translations-list dt a"#)?;
 
     let mut book_names = Vec::new();
     let mut book_urls = Vec::new();
@@ -66,12 +69,10 @@ fn parse_page(url: &str, dump_dir_name: &str) {
     document
         .select(&selector)
         .map(|a| {
-            book_names.push(a.text().collect::<Vec<_>>().join("").trim().to_owned());
-            book_urls.push(format!(
-                "{}{}",
-                SITE_BASE_URL,
-                a.value().attr("href").unwrap().replace("/trans/", "/")
-            ));
+            if let Some(url) = a.value().attr("href") {
+                book_names.push(a.text().collect::<Vec<_>>().join("").trim().to_owned());
+                book_urls.push(format!("{}{}", &SITE_BASE_URL, url.replace("/trans/", "/")));
+            };
         })
         .for_each(drop);
     println!("{:#?}", book_names);
@@ -82,9 +83,10 @@ fn parse_page(url: &str, dump_dir_name: &str) {
         .zip(book_urls.iter())
         .map(|(name, url)| parse_book(name, url, &dump_dir_name))
         .for_each(drop);
+    Ok(())
 }
 
-fn parse_book(name: &str, url: &str, dump_dir_name: &str) {
+fn parse_book(name: &str, url: &str, dump_dir_name: &str) -> Result<(), Box<dyn Error>> {
     println!("Book: {}", name);
     println!("Book url: {}", url);
 
@@ -92,29 +94,24 @@ fn parse_book(name: &str, url: &str, dump_dir_name: &str) {
     let book_file_url = format!("{}.txt", url);
 
     let book_dir_name = format!("{}/{}", dump_dir_name, name);
-    DirBuilder::new()
-        .create(&book_dir_name)
-        .expect("Unable to create directory");
+    DirBuilder::new().create(&book_dir_name)?;
 
-    let resp = reqwest::blocking::get(about_page_url).expect("Server error");
-    let text = resp.text().expect("Unable to read response");
+    let text = reqwest::blocking::get(&about_page_url)?.text()?;
 
     let document = Html::parse_document(&text);
-    let selector = Selector::parse(r#"#about-translation blockquote"#).expect("Selector error");
+    let selector = Selector::parse(r#"#about-translation blockquote"#)?;
     let blockquote = match document.select(&selector).next() {
         Some(a) => a.text().collect::<Vec<_>>().join("").trim().to_owned(),
         None => String::new(),
     };
 
     fs::write(
-        format!("{}/about.txt", book_dir_name),
+        format!("{}/about.txt", &book_dir_name),
         format!("URL - {}\n{}", url, blockquote).as_bytes(),
-    )
-    .expect("Unable to write file");
+    )?;
 
-    let resp = reqwest::blocking::get(book_file_url).expect("Server error");
-    let text = resp.text().expect("Unable to read response");
+    let text = reqwest::blocking::get(&book_file_url)?.text()?;
 
-    fs::write(format!("{}/book.txt", book_dir_name), text.as_bytes())
-        .expect("Unable to write file");
+    fs::write(format!("{}/book.txt", &book_dir_name), text.as_bytes())?;
+    Ok(())
 }
